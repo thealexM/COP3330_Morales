99588471fd3cc1b3061382872000eae68230fb1c
thealexM <70339371+thealexM@users.noreply.github.com>
2020-11-16 22:05:08 -0500

    Update App.java

--- Morales_p4/src/App.java
+++ Morales_p4/src/App.java
@@ -1,254 +1,254 @@
 import java.util.Scanner;
 
 public class App {
-
+   
     static Scanner in = new Scanner(System.in);
-
+    
     public static void main(String[] args) {
-
+        
         int selection,choice;
-
-        String input;
-
+        
         do {
+         
             Scanner in = new Scanner(System.in);
-
+           
             do {
                 System.out.print(MainMenu());
-
+                
                 selection = MenuHandler(StringToInt(in.nextLine()),3);
-
+               
                 screenClear();
+           
             } while(selection == -1);
-
+           
             if (selection == 1) {
-
-                System.out.print("A task has been made.");
+                
+                System.out.print("\nA task has been created.\n");   
             }
-
+           
             if (selection == 2) {
-
+             
+                String FileName;
+             
                 do {
-
-                    do {
-
-                        System.out.print("Enter a file name (no extensions allowed): ");
-
-                        input = TaskList.ValidateFileName(in.nextLine() + TaskList.extension);
-
-                    } while (input.length() == 0);
-                } while (!TaskList.ReadProtection(input));
-
-                TaskList.LoadFile(TaskList.ReadFile(input));
+                
+                    do{
+                       
+                        System.out.print("\nEnter a file name (no extensions allowed): ");
+                       
+                        FileName = in.nextLine() + TaskList.extension;
+                    
+                    }while (!TaskList.ValidateFileName(FileName));
+                
+                } while (!TaskList.ReadProtection(FileName));
+                
+                boolean isValidFile = TaskList.LoadFile(TaskList.ReadFile(FileName));
+               
+                if(!isValidFile){
+                 
+                    System.out.print("\nThe file specified is not a valid task file.\nCreating new list.\n");
+                   
+                    TaskList.ClearTaskList();
+                }
             }
-
+            
             if(selection != 3) {
-
+              
                 do {
-
+                   
                     do {
-
+                       
                         screenClear();
-
+                       
                         System.out.print(OpsMenu());
-
+                      
                         choice = MenuHandler(StringToInt(in.nextLine()), 8);
-
+                   
                     } while (choice == -1);
-
+                   
                     OpsAction(choice);
-
+               
                 } while (choice != 8);
             }
-
         } while (selection != 3);
-
     }
-
     protected static void OpsAction(int choice) {
-
+       
         String title, description, due_date;
-
+       
         int index;
-
+       
         int[] key;
-
+       
         if(choice == 1) {
-
+           
             System.out.print(TaskList.PrintableList());
+           
             System.out.print("\nPress enter to continue.\n>");
+           
             in.nextLine();
         }
-
         if(choice == 2) {
+         
             do{
-                System.out.print("Task title: ");
-
+             
+                System.out.print("\nTask title: ");
+             
                 title = in.nextLine();
-
-                System.out.print("Task description: ");
-
+             
+                System.out.print("\nTask description: ");
+             
                 description = in.nextLine();
-
-                System.out.print("Task due date (YYYY-MM-DD): ");
-
+               
+                System.out.print("\nTask due date (YYYY-MM-DD): ");
+               
                 due_date = in.nextLine();
-
-            } while (!TaskList.ValidateTask(title, due_date, description));
-
+           
+            } while (!TaskList.ValidateTask(title, due_date));
+           
             TaskList.addTask(title, description, due_date);
         }
-
         if(choice == 3) {
-
+         
             System.out.print(TaskList.PrintableList());
-
-            System.out.print("\nSelect a task to edit: ");
-
+         
+            System.out.print("\n\nSelect a task to edit: ");
+         
             do{
-
+               
                 index = MenuHandler(StringToInt(in.nextLine()),TaskList.List.size());
-
+            
             } while (index == -1);
-
+            
             do{
-
-                System.out.print("Task title: ");
-
+               
+                System.out.print("\nTask title: ");
+               
                 title = in.nextLine();
-
-                System.out.print("Task description: ");
-
+                
+                System.out.print("\nTask description: ");
+                
                 description = in.nextLine();
-
-                System.out.print("Task due date (YYYY-MM-DD): ");
-
+               
+                System.out.print("\nTask due date (YYYY-MM-DD): ");
+               
                 due_date = in.nextLine();
-
-            } while (!TaskList.ValidateTask(title, due_date, description));
-
-            TaskList.editVisibleTask(title, description, due_date, index);
+            
+            } while (!TaskList.ValidateTask(title, due_date));
+           
+            TaskList.editTask(title, description, due_date, index);
         }
-
         if(choice == 4) {
-
+           
             screenClear();
-
+           
             System.out.print(TaskList.PrintableList());
-
-            System.out.print("\nSelect a task to remove: ");
-
+           
+            System.out.print("\n\nSelect which task to remove: ");
             do{
-
+                
                 index = MenuHandler(StringToInt(in.nextLine()),TaskList.List.size());
-
+            
             } while(index == -1);
-
-            TaskList.removeTask(index);
+            
+            TaskList.removeTask(index-1);
         }
-
         if(choice == 5 || choice == 6) {
-
+           
             screenClear();
-
+           
             boolean complete = false;
-
+           
             if(choice == 6) complete = true;
-
+           
             System.out.print(TaskList.PartialTasks(complete));
-
+            
             key = TaskList.GenerateCompletionKey(complete);
-
+           
             if(key.length > 0) {
-
+               
                 do {
+                    
                     index = MenuHandler(StringToInt(in.nextLine()), key.length);
 
+               
                 } while (index == -1);
-
-                TaskList.editInvisibleTask(key[index-1],!complete);
+               
+                TaskList.editCompletionStatus(key[index-1],!complete);
             }
         }
-
         if(choice == 7) {
-
+          
             screenClear();
-
+          
             TaskList.PrintSavePrompt();
-
-            String FileName = TaskList.ValidateFileName(in.nextLine()) + TaskList.extension;
-
+          
+            String FileName;
+           
+            do{
+               
+                System.out.print("\nEnter file name (no extension): ");
+               
+                FileName = in.nextLine() + TaskList.extension;
+           
+            }while (!TaskList.ValidateFileName(FileName));
+           
             TaskList.CreateFile(FileName);
-
+            
             TaskList.SaveTaskList(TaskList.AmassListInfo(), FileName);
         }
-
         if(choice == 8) {
-
-            ClearTaskList();
+            TaskList.ClearTaskList();
         }
     }
-    protected static void ClearTaskList() {
-
-        for(int index = TaskList.List.size() - 1; index >= 0; index --) TaskList.List.remove(index);
-    }
     protected static String     OpsMenu() {
-
-        return "\nList Operation Menu\n" +
-                "-------\n" +
-
+      
+        return "\n\nList Operation Menu\n" +
+                "---------\n\n" +
+               
                 "1) view the list\n" +
-
                 "2) add an item\n" +
-
                 "3) edit an item\n" +
-
                 "4) remove an item\n" +
-
-                "5) mark an item as completed\n" +
-
-                "6) unmark an item as completed\n" +
-
-                "7) save the current list\n" +
-
-                "8) quit to the main menu\n\nEnter your selection: ";
+                "5) mark item as completed\n" +
+                "6) unmark item as completed\n" +
+                "7) save current list\n" +
+                "8) quit to main menu\n\nEnter your selection: ";
     }
     protected static String    MainMenu() {
-
+      
         screenClear();
-
-        return "\nMain Menu\n---------\n" +
-
-                "1) create a new list\n" +
-
-                "2) load an existing list\n" +
-
-                "3) quit\n\nEnter your selection: ";
+       
+        return "\n\nMain Menu\n---------\n\n" +
+                "1) Create a new list\n" +
+                "2) Load  existing list\n" +
+                "3) Quit\n\nEnter your selection: ";
     }
     protected static int      MenuHandler(int choice, int length) {
-
+        
         if (choice <= length && choice > 0) return choice;
-
-        System.out.print("\nThat was not a listed option. Try again.\n");
-
+       
+        System.out.print("\nThat wasn't a listed option. Please Try again.\n");
+        
         return -1;
     }
     protected static int StringToInt(String string){
-
-        int number = 0, digit = 0;
-
-        for(int index = 0; index < string.length(); index++)
-
+       
+        int number = 0, digit;
+        
+        for(int index = 0; index < string.length(); index++){
+           
             digit = string.charAt(index) - 48;
-
-        if(digit > 9 || digit < 0) return 0;
-
-            number += digit;
-
+            
+            if(digit > 9 || digit < 0) return 0;
+           
+            number = 10*number + digit;
+        }
+        
         return number;
     }
-
     protected static void screenClear(){
-        System.out.print("\n\n\n\n\n\n\n\n\n\n");
+       
+        System.out.print("\n\n\n\n\n\n\n\n\n");
     }
 }
